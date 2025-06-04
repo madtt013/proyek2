a8f95bacedf193176bf3efeac98ef960b27a3d65
hay013 <haymatt013@gmail.com>
2025-06-05 01:49:55 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Penguin Dashboard", layout="wide")
+st.title("🐧 Dashboard Analisis Data Penguin")
+st.markdown("Selamat datang di dashboard interaktif untuk eksplorasi data, pelatihan model, dan prediksi ukuran penguin.")
