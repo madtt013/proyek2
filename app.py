33ac1b71b78a34246e4e2c2953fed872df274211
hay013 <haymatt013@gmail.com>
2025-06-18 16:35:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.markdown("Selamat datang di dashboard interaktif untuk eksplorasi data, pelat
 st.subheader("Anggota Kelompok 8")
 st.write("1.(2304030025) Haydar Ahmad")
 st.write("2.(4101422078) Nila Malika Rizqi")
-st.writw("3.(4101422084) Laily Arfina Sari")
+st.write("3.(4101422084) Laily Arfina Sari")
 st.write("4.(4101422075) Anlistya Mega Annasti")
