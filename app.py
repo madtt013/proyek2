e290e5fff6984396f0761478b9cff666eae2829a
hay013 <haymatt013@gmail.com>
2025-06-18 16:35:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Penguin Dashboard", layout="wide")
 st.title("🐧 Dashboard Analisis Data Penguin")
 st.markdown("Selamat datang di dashboard interaktif untuk eksplorasi data, pelatihan model, dan prediksi ukuran penguin.")
-st.subtitle("Anggota Kelompok 8")
+st.subheader("Anggota Kelompok 8")
 st.write("1.(2304030025) Haydar Ahmad")
 st.write("2.(4101422078) Nila Malika Rizqi")
 st.writw("3.(4101422084) Laily Arfina Sari")
